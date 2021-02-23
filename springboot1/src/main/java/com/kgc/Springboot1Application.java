38b633cfcf36eaa07a73c194e3c368e79d46187f
zhangsan <zhangsan@126.com>
2021-02-23 16:25:15 +0800

    idea update for commit 2

--- springboot1/src/main/java/com/kgc/Springboot1Application.java
+++ springboot1/src/main/java/com/kgc/Springboot1Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Springboot1Application {
-
+//123
     public static void main(String[] args) {
         SpringApplication.run(Springboot1Application.class, args);
     }
