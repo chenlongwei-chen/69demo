ba747e02d335f2fbba81e3e7b402f003bc5fd90c
chenlongwei-chen <79238262+chenlongwei-chen@users.noreply.github.com>
2021-02-23 11:15:55 +0800

    Update and rename springboot1/src/main/java/com/kgc/controller/HelloController.java to springboot1/src/主要的/爪哇/com/公斤/控制器/HelloController.java

--- springboot1/src/主要的/爪哇/com/公斤/控制器/HelloController.java
+++ springboot1/src/主要的/爪哇/com/公斤/控制器/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//控制器
 @RestController
 public class HelloController {
 
