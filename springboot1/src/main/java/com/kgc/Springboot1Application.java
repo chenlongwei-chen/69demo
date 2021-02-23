bd2a766ced1b6c39189d5868ca135ba10eae55b2
chenlongwei-chen <79238262+chenlongwei-chen@users.noreply.github.com>
2021-02-23 11:15:55 +0800

    Update and rename springboot1/src/main/java/com/kgc/controller/HelloController.java to springboot1/src/主要的/爪哇/com/公斤/控制器/HelloController.java

--- springboot1/src/main/java/com/kgc/Springboot1Application.java
+++ springboot1/src/main/java/com/kgc/Springboot1Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Springboot1Application {
 
     public static void main(String[] args) {
+        //awew
         SpringApplication.run(Springboot1Application.class, args);
     }
 
